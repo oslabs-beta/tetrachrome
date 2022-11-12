3feb2cf9cfabc5996fff017e5e8b5f49a38de9ba
Terry Park <terrypark@terrys-air.lan>
2022-11-12 13:19:59 -0800

    Co-authored-by: Terry Park <37316422+tjpark95@users.noreply.github.com> Co-authored-by: Marco Xu <106225683+marcoxulin@users.noreply.github.com> Co-authored-by: Risa Ochiai <06301648+risa-10@users.noreply.github.com> Co-authored-by: Judy Wu <41937921+judywuxingyi@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// serve index.html
+app.use('/', express.static(path.join(__dirname, '../client/')));
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('Page not found'));
+
+// error handler
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' }
+    };
+    const errorObj = Object.assign(defaultErr, err);
+    console.log(errorObj.log);
+    return JSON.stringify(errorObj.status, errorObj.message);
+});
+
+// Express app will use port 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname,'build'),
+  },
+  resolve: {
+    extensions: ['.js','.jsx'],
+  },
+  
+  module: {
+    rules: [
+      {
+        test:/\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        },
+      },
+      {
+        test: /\.(s?(a|c)ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html'
+    }),
+  ],
+  
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './build')
+    },
+    proxy: {
+      '/': 'http://localhost:3000',
+    }
+  },
+};
