e992f44b2feaa894beb6ca77d025390c1b1a168b
Judy <judywuxingyi@gmail.com>
2022-11-30 23:01:12 -0500

    updated nav links and frontend container

--- client/containers/FrontendContainer.js
+++ client/containers/FrontendContainer.js
@@ -1,57 +1,47 @@
-import React from 'react';
-import Tree from 'react-d3-tree';
+import React, { useState, useEffect } from "react";
 
 const FrontendContainer = ({ frame }) => {
+  console.log('inside FrontendContainer');
+
+  const [tree, setTree] = useState({});
+
   // get document of app-frame
   const frameContent = frame.contentWindow.document;
-  // let tree = {
-  //   name:'',
-  //   children: []
-  // };
-
   // find all nodes in within document body of app-frame
   const allNodes = frameContent.querySelectorAll('*');
-  // const allNodes = document.querySelectorAll('*');
-
   // traverse allNodes to find the root node
   let rootNode;
-
   allNodes.forEach(node => {
     if (node._reactRootContainer) rootNode = node._reactRootContainer._internalRoot.current;
-  });
-  console.log('root node --> ', rootNode);
-  
+  });  
   
   // track + perform work on fiber node
-  const performUnitOfWork = (fiber) => {
-    if (fiber.type !== null) console.log(`${fiber.type} start`);
-    console.log('inside performUnitOfWork');
-    
-    if (fiber.child) {
-      return fiber.child;
-    }
+  const getNext = (fiber) => {
+    // console.log('inside performUnitOfWork');  
+    if (fiber.child) return fiber.child;
     while (fiber) {
-      // if (fiber.type !== null) console.log(`${fiber.type} end`);
-      if (fiber.sibling) {
-        return fiber.sibling;
-      }
+      if (fiber.sibling) return fiber.sibling;
       fiber = fiber.return;
     }
   }
   // traverse component tree
   const traverse = (nextNode) => {
+    console.log('inside traverse');
     while (nextNode) {
-      nextNode = performUnitOfWork(nextNode);
+      console.log('stateNode -->', nextNode.stateNode);
+      nextNode = getNext(nextNode.stateNode);
     }
-    if (!nextNode) {
-      console.log('End of traversal');
-    }
+    if (!nextNode) console.log('End of traversal');
   }
 
+  useEffect(() => {
+    traverse(rootNode);
+  }, []);
+
   return (
-    <div id="tree-wrapper">
-      <Tree data={rootNode} />
-    </div>
+    <>
+      <h1>hello</h1>
+    </>
   );
 }
 
