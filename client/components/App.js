68e197facfa0aeb81a09bbbcd677ac956aa28f4c
Terry Park <terrypark.jh@gmail.com>
2023-01-11 19:52:01 -0800

    websocket and frontend container changes

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
+//websocket package
+import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
+//url for websockets
+const WS_URL = 'ws://127.0.0.1:3030';
+
 import Nav from "./Nav";
 
 import MainContainer from "../containers/MainContainer";
@@ -10,9 +15,16 @@ import MetricsContainer from "../containers/MetricsContainer";
 import GettingStarted from "./GettingStarted";
 import Docs from "./Docs";
 
+
+
 function App() {
   const [frame, setFrame] = useState('');
 
+  useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log("websocket connection established");
+    }
+  });
 
   useEffect(() => {
     const currentFrame = document.getElementById('app-frame');
@@ -24,8 +36,10 @@ function App() {
     <>
       <Nav />
       <div id="main-container">
-        <Routes>
-          <Route path="/blueprint" element={<MainContainer />} />
+        <MainContainer />
+      </div>
+      <div id="side-container">
+      <Routes>
           <Route path="/blueprint/frontend" element={<FrontendContainer frame={frame} />} />
           <Route path="/blueprint/backend" element={<BackendContainer />} />
           <Route path="/blueprint/metrics" element={<MetricsContainer />} />
