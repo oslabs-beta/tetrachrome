8bc403eb8c48392758179ad0b72fc1f6ac01dec6
Marco Xu <106225683+marcoxulin@users.noreply.github.com>
2023-01-12 17:02:19 -0800

    socket.io with backend route stack rendering

--- client/components/App.js
+++ client/components/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Routes, Route } from "react-router-dom";
-
+import { io } from "socket.io-client";
 import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
-const WS_URL = 'ws://127.0.0.1:3030';
+const WS_URL = "ws://127.0.0.1:3030";
 
 import Nav from "./Nav";
 
@@ -14,19 +14,34 @@ import GettingStarted from "./GettingStarted";
 import Docs from "./Docs";
 
 function App() {
-  const [frame, setFrame] = useState('');
+  const [frame, setFrame] = useState("");
+  const socketRef = useRef();
+  const [routesStack, setRoutes] = useState([])
+  // useWebSocket(WS_URL, {
+  //   onOpen: () => {
+  //     console.log("websocket connection established");
+  //   }
+  // });
 
-  useWebSocket(WS_URL, {
-    onOpen: () => {
-      console.log("websocket connection established");
-    }
-  });
+  function connect() {
+    // send a message to the server
+    socketRef.current = io("ws://localhost:3030");
+    socketRef.current.emit("hello from blueprint", "hello from blueprint");
 
+    // receive a message from the server
+    socketRef.current.on("hello from user server", (message) => {
+      console.log(message);
+    });
+    socketRef.current.on("route stack", (routes) => {
+      setRoutes(routes);
+    });
+  }
   useEffect(() => {
-    const currentFrame = document.getElementById('app-frame');
+    const currentFrame = document.getElementById("app-frame");
     // const currentFrame = await fetch('http://localhost:8080');
     setFrame(currentFrame);
-  },[]);
+    connect();
+  }, []);
 
   return (
     <>
@@ -34,14 +49,19 @@ function App() {
       <div id="main-container">
         <Routes>
           <Route path="/blueprint" element={<MainContainer />} />
-          <Route path="/blueprint/frontend" element={<FrontendContainer frame={frame} />} />
-          <Route path="/blueprint/backend" element={<BackendContainer />} />
+          <Route
+            path="/blueprint/frontend"
+            element={<FrontendContainer frame={frame} />}
+          />
+          <Route path="/blueprint/backend" element={<BackendContainer routes={routesStack}/>} />
           <Route path="/blueprint/metrics" element={<MetricsContainer />} />
-          <Route path="/blueprint/gettingstarted" element={<GettingStarted />} />
+          <Route
+            path="/blueprint/gettingstarted"
+            element={<GettingStarted />}
+          />
           <Route path="/blueprint/docs" element={<Docs />} />
         </Routes>
       </div>
-
     </>
   );
 }
--- client/containers/BackendContainer.js
+++ client/containers/BackendContainer.js
@@ -3,7 +3,7 @@ import expressObject from "../../build/expressObject.json";
 // remember to change the location(path) of the file when bundling
 // the file should be in the same folder('bundle') as the bundlesjs/html
 
-const BackendContainer = () => {
+const BackendContainer = ({routes}) => {
 //   let route;
 //   let routes = [];
 //   expressObject.forEach((middleware) => {
@@ -18,22 +18,23 @@ const BackendContainer = () => {
 //       });
 //     }
 //   });
-function availableRoutesString() {
-    return expressObject
-      .filter(r => r.route)
-      .map(r => Object.keys(r.route.methods)[0].toUpperCase().padEnd(7) + r.route.path)   
-  }
-const routesArray = availableRoutesString();
+// function availableRoutesString() {
+//     return expressObject
+//       .filter(r => r.route)
+//       .map(r => Object.keys(r.route.methods)[0].toUpperCase().padEnd(7) + r.route.path)   
+//   }
+// const routesArray = availableRoutesString();
 const routeList = []
 
-for (let i = 0; i < routesArray.length; i++) {
-    routeList.push(<h1>{routesArray[i]}</h1>)
+// for (let i = 0; i < routesArray.length; i++) {
+//     routeList.push(<h1>{routesArray[i]}</h1>)
+// }
+console.log(routes);
+for (let i = 0; i < routes.length; i++) {
+    routeList.push(<h1>{`method: ${routes[i].method}, path: ${routes[i].path}`}</h1>)
 }
 
-//   console.log(typeof availableRoutesString());
-//   console.log(availableRoutesString());
-//   console.log(route);
-//   console.log(routes);
+
 
     // console.log(expressObject)
   return <>
