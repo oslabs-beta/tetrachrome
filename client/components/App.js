8bf2bdfe19f90737b40fbdb1f752d0bac3f0efd7
Terry Park <terrypark.jh@gmail.com>
2023-01-14 09:52:53 -0800

    websocket-client changes and Backendcontainer array change

--- client/components/App.js
+++ client/components/App.js
@@ -1,34 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Routes, Route } from "react-router-dom";
-
-//websocket package
-import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
-//url for websockets
-const WS_URL = 'ws://127.0.0.1:3030';
-
 import Nav from "./Nav";
-
 import MainContainer from "../containers/MainContainer";
 import FrontendContainer from "../containers/FrontendContainer";
 import BackendContainer from "../containers/BackendContainer";
 import MetricsContainer from "../containers/MetricsContainer";
 import GettingStarted from "./GettingStarted";
 import Docs from "./Docs";
-
+import { io } from "socket.io-client";
 
 
 function App() {
   const [frame, setFrame] = useState('');
+  //ref hook for socket.io
+  const socketRef = useRef();
+  const [routesStack, setRoutes] = useState([])
 
-  useWebSocket(WS_URL, {
-    onOpen: () => {
-      console.log("websocket connection established");
-    }
-  });
+  //function that connects to the websocket and contains events listeners
+  function connectSocketIO() {
+    // send a message to the server
+    socketRef.current = io("ws://localhost:3030");
+    socketRef.current.emit("hello from blueprint", "hello from blueprint");
+
+    // receive a message from the server
+    socketRef.current.on("hello from user server", (message) => {
+      console.log(message);
+    });
+    socketRef.current.on("route stack", (routes) => {
+      setRoutes(routes);
+    });
+  }
 
   useEffect(() => {
     const currentFrame = document.getElementById('frame');
     setFrame(currentFrame);
+    connectSocketIO();
   }, []);
 
   return (
@@ -40,7 +46,7 @@ function App() {
       <div id="side-container">
         <Routes>
           <Route path="/blueprint/frontend" element={<FrontendContainer frame={frame}/>} />
-          <Route path="/blueprint/backend" element={<BackendContainer />} />
+          <Route path="/blueprint/backend" element={<BackendContainer routes={routesStack}/>} />
           <Route path="/blueprint/metrics" element={<MetricsContainer />} />
           <Route path="/blueprint/gettingstarted" element={<GettingStarted />} />
           <Route path="/blueprint/docs" element={<Docs />} />
